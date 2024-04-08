7e9bddac3875706e26e6e7c5209a5a0bf4d866ec
b0bhat <ryanwu0308@gmail.com>
2024-04-08 15:34:37 -0700

    change path

--- CTU-CHB/DCNN/test.py
+++ CTU-CHB/DCNN/test.py
@@ -7,14 +7,14 @@ import tensorflow as tf
 
 from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score, confusion_matrix
 
-with open('model_architecture.json', 'r') as json_file:
+with open('CTU-CHB/DCNN/model_architecture.json', 'r') as json_file:
     architecture = json.load(json_file)
 model = tf.keras.models.model_from_json(architecture)
-model.load_weights('model.weights.h5')
+model.load_weights('CTU-CHB/DCNN/model.weights.h5')
 
-with gzip.open('artifacts/test_images.npy.gz', 'rb') as f:
+with gzip.open('CTU-CHB/DCNN/artifacts/test_images.npy.gz', 'rb') as f:
     test_images = np.load(f)
-test_labels = np.load('artifacts/test_labels.npy')
+test_labels = np.load('CTU-CHB/DCNN/artifacts/test_labels.npy')
 
 predictions = model.predict(test_images)
 predicted_classes = np.argmax(predictions, axis=1)
